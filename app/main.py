8c20ef55eabeabdf60c05ded8aca9ceffef50466
Federica <f.l.vinella@uu.nl>
2022-03-09 16:53:41 +0100

    hello

--- app/main.py
+++ app/main.py
@@ -8,35 +8,37 @@ app = Flask(__name__)
 
 @app.route("/bot", methods=["POST"])
 def bot():
-    resp = MessagingResponse()
-    msg = resp.message()
-    responded = False
-
+    """ instantiate lists """
     reserved = ["DATE", "NUTRIENT"]
     ner_input = []  # empty list
+    
+    """ get the response of the bot"""
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False # set the bot response to false
 
+    """get the message of the user"""
     incoming_msg = request.values.get("Body", "").lower()  # inp
     spacy_res = nlp_ner(incoming_msg)  # process the input with spacy
 
+    """extract entities from the spacy obj and append to empty list"""
     for ent in spacy_res.ents:
         ner_input.append(ent.label_)
+    
+    """if the list is not empty check which entity is missing and save it to a list"""
     if ner_input:
         missing = [i for i in reserved if i not in ner_input]
-        try:
-            if missing:
+        if missing:
                 # TODO: present the missing entities in a string format
                 msg.body(f"this is missing: {missing}")
-        except:
-            pass
-    else:
-        # for each enetity that we have taken from the user input
-        for ent in spacy_res.ents:
-            if ent.label_ == "DATE":
-                date = ent.text
-            if ent.label_ == "NUTRIENT":
-                nutrient = ent.text
-        msg.body(date, nutrient)
-    responded = True
+        else:
+            for ent in spacy_res.ents:
+                if ent.label_ == "DATE":
+                    date = ent.text
+                if ent.label_ == "NUTRIENT":
+                    nutrient = ent.text
+                msg.body(date, nutrient)
+    responded = True 
     if not responded:
         msg.body("I only know about famous quotes and cats, sorry!")
     return str(resp)
