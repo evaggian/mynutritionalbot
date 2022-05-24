e79a63aa48e556f3af3fadf4f161271a73296ad5
Evangelia Giannikou <e.giannikou@uu.nl>
2022-05-24 15:15:18 +0200

    fixed stupid date bug

--- app/date.py
+++ app/date.py
@@ -1,9 +1,17 @@
 from datetime import datetime
 import parsedatetime
+import dateparser
 
+# retrieve date text and convert it into date type
+# parsedatetime library doesn't work with numerical dates, it works only for relative dates
+# more info can be found here: https://dateparser.readthedocs.io/en/latest/
 
 def get_date(date):
-    cal = parsedatetime.Calendar()
-    time_struct, parse_status = cal.parse(date)
-    
-    return datetime(*time_struct[:6]).date()
+
+    if "-" in date or "/" in date:                      # if these characters exist in the string
+        return dateparser.parse(date).date()            # the date provided is numerical and use the 'dateparser' library
+    else:
+        cal = parsedatetime.Calendar()                  # if the date provided is relative
+        time_struct, parse_status = cal.parse(date)     # then use the 'parsedatetime' library
+        
+        return datetime(*time_struct[:6]).date()
--- app/test.py
+++ app/test.py
@@ -1,14 +1,4 @@
-import datetime
+from date import get_date
 
-list = [{'Avocado - Avocado, 1 medium': 3.0}, {'Coles lamp rump steaks - Lamp steak, 1 steak': 47.0}]
-
-top = 0 
-top_food = None
-for index in range(len(list)):
-    for key in list[index]:
-        if list[index][key] > top:
-            top = list[index][key]
-            top_food = list[index]
-        #print(list[index][key])
-
-print(top_food)
+r = get_date("yesterday")
+print(r)
